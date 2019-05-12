60457b47a9d81b292a1502b6cb04dbae36a64310
Junior <junior.arodriguez@icloud.com>
2019-05-12 13:09:43 -0500

    created the calculator app with button components and display components

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = function(props) {
+    return (
+        <div className="number-button">
+            <button className={props.buttonStyle}>{props.text}</button>
+        </div>        
+    )        
+};
+
+export default NumberButton;
